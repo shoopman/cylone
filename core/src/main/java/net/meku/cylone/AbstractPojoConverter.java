a4c2eda7bccd0c59a1eb5ac3bb0a597cc8e4d550
Bin Liang <shoopman917@gmail.com>
2019-08-30 11:03:35 +0800

    added build scripts

--- core/src/main/java/net/meku/cylone/AbstractPojoConverter.java
+++ core/src/main/java/net/meku/cylone/AbstractPojoConverter.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 public abstract class AbstractPojoConverter<T, S>
                                            implements PojoConverter<T, S> {
-    
+
     /**
      * 创建空的目标实例，子类可覆盖
      *
@@ -22,45 +22,45 @@ public abstract class AbstractPojoConverter<T, S>
         Class<T> type =
                       (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
         try {
-            return type.newInstance();
+            return type.getDeclaredConstructor().newInstance();
         }
         catch (Exception e) {
             throw new IllegalStateException(e);
         }
     }
-    
+
     @Override
     public T toPojo(S source) {
         if (source == null) {
             return null;
         }
-        
+
         T target = createEmptyTarget(source);
         toPojo(source, target);
-        
+
         return target;
     }
-    
+
     @Override
     public List<T> toList(List<S> sourceList) {
         if (sourceList == null) {
             return Collections.emptyList();
         }
-        
+
         List<T> targetList = new ArrayList<>(sourceList.size());
         sourceList.forEach(source -> targetList.add(toPojo(source)));
         return targetList;
     }
-    
+
     @Override
     public Page<T> toPage(Page<S> sourcePage) {
         if (sourcePage == null) {
             return null;
         }
-        
+
         List<T> targetList = toList(sourcePage.getContent());
         Page<T> targetPage = new ConvertedPage<>(sourcePage, targetList);
         return targetPage;
     }
-    
+
 }
